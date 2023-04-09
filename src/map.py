66a75c0a42fa58f8cd5f0d86496b57079e4a9cf6
Jelly <Jellybob2016@outlook.com>
2023-04-09 19:36:58 +0100

    Implement temp multipliers

--- src/map.py
+++ src/map.py
@@ -1,5 +1,8 @@
 __all__ = ["Map"]
 
+import asyncio
+
+from pygame import time
 from pygame.rect import Rect
 from pygame.surface import Surface
 from .entities.player import Player
@@ -8,49 +11,35 @@ from .entities.bullet_controller import BulletController
 from .colour import Colour
 from typing import Dict
 
+from .playarea import PlayArea
+
 
 class Map:
-    __slots__ = ("player", "enemy_controller", "bullet_controller", "play_area", "multiplier", "play_area_size")
+    __slots__ = ("player", "enemy_controller", "bullet_controller", "play_area", "current_play_area")
 
     player: Player
     enemy_controller: EnemyController
     bullet_controller: BulletController
-    play_area: Rect
-    multiplier: float
-    play_area_size: Dict[str, float]
+    play_area: PlayArea
+    current_play_area: Rect
 
     def __init__(self) -> None:
         self.player = Player()
         self.enemy_controller = EnemyController()
         self.bullet_controller = BulletController()
 
-        self.play_area_size = {
-            "width": 1600,
-            "height": 900
-        }
-
-        self.play_area = Rect(0, 0, self.play_area_size["width"], self.play_area_size["height"])
-        self.play_area.center = (800, 450)
-        self.multiplier = 3.0
+        self.play_area = PlayArea(900, 600, 3.0)
+        self.current_play_area = self.play_area.play_area
 
     def update(self, frame_time: float, **kwargs) -> None:
-        temp_multiplier = (frame_time * self.multiplier * 10)
+        play_area = self.play_area.update(frame_time, self.current_play_area)
+        self.current_play_area = play_area
 
-        self.play_area_size["width"] -= temp_multiplier * (16/25) * 1.5
-        self.play_area_size["height"] -= temp_multiplier * (9/25) * 1.5
-
-        self.play_area = Rect(
-            round((1600 - self.play_area_size["width"]) / 2),
-            round((900 - self.play_area_size["height"]) / 2),
-            self.play_area_size["width"],
-            self.play_area_size["height"]
-        )
-
-        self.player.update(frame_time, play_area=self.play_area)
+        self.player.update(frame_time, play_area)
 
         self.enemy_controller.update(
             frame_time,
-            play_area=self.play_area,
+            play_area=play_area,
             player=self.player,
             bullet_controller=self.bullet_controller,
             **kwargs
@@ -59,7 +48,7 @@ class Map:
         self.bullet_controller.update(frame_time, player_sprite=self.player.sprite, **kwargs)
 
     def draw(self, window: Surface) -> None:
-        window.fill(Colour.White, self.play_area)
+        window.fill(Colour.White, self.current_play_area)
 
         self.player.draw(window)
         self.enemy_controller.draw(window)
@@ -67,7 +56,7 @@ class Map:
 
     def has_player_died(self) -> bool:
 
-        if self.play_area.w == 0 or self.play_area.h == 0:
+        if self.current_play_area.w == 0 or self.current_play_area.h == 0:
             return True
         else:
             return False
