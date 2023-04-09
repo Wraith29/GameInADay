057f24e0ad52e00fb001b35a3c1eb1c6aa5e633a
Rosia E Evans <Willowro@outlook.com>
2023-04-09 20:05:48 +0100

    adds player bullet

--- src/entities/bullet.py
+++ src/entities/bullet.py
@@ -20,16 +20,19 @@ class Bullet(Sprite):
                  movement_modifier: Callable[[Vector2, float], Vector2] | None = None
                 ) -> None:
         Sprite.__init__(self)
-        self.sprite = "sprites/Bullet.png"
-        self.size = 20
-        self.image = transform.scale(image.load(self.sprite), (int(self.size), int(self.size)))
-
-        self.rect = self.image.get_rect()
-        self.rect.center = (int(start_position.x), int(start_position.y))
 
         self.position = start_position
         self.movement_vector = movement_vector
         self.movement_modifier = movement_modifier
+
+        self.set_texture("sprites/EnemyBullet.png", start_position.x, start_position.y)
+
+    def set_texture(self, texture: str, x_pos: float, y_pos: float) -> None:
+        self.sprite = texture
+        self.size = 20
+        self.image = transform.scale(image.load(self.sprite), (int(self.size), int(self.size)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (int(x_pos), int(y_pos))
 
     def update(self, frame_time: float, **kwargs) -> None:
         self.position += self.movement_vector * frame_time * 100
@@ -38,3 +41,12 @@ class Bullet(Sprite):
             self.movement_vector = self.movement_modifier(self.movement_vector, frame_time)
 
         self.rect.center = (int(self.position.x), int(self.position.y))
+     
+
+class PlayerBullet(Bullet):
+    def __init__(self, start_position: Vector2, 
+                 movement_vector: Vector2, 
+                 movement_modifier: Callable[[Vector2, float], Vector2] | None = None
+                 ) -> None:
+        super().__init__(start_position, movement_vector, movement_modifier)
+        self.set_texture("sprites/PlayerBullet.png", start_position.x, start_position.y)
