1bdb4dc509ebc5d676772fd557eb0b68656b652d
Isaac Naylor <i.acnaylor@gmail.com>
2023-04-10 16:52:40 +0100

    fixed spamming bug

--- src/entities/bullet_generators/player_bullet_generator.py
+++ src/entities/bullet_generators/player_bullet_generator.py
@@ -9,15 +9,17 @@ from ..bullets.player_bullet import PlayerBullet
 
 
 class PlayerBulletGenerator(BulletGenerator):
-    def __init__(self, cooldown : int | float = 1, bullet_type: type = PlayerBullet):
+    def __init__(self, cooldown: int | float = 1, bullet_type: type = PlayerBullet):
         BulletGenerator.__init__(self, cooldown, bullet_type)
 
     def update(self, frame_time: float, position: Vector2, bullet_controller: BulletController, **kwargs) -> None:
-        self.total_time -= frame_time
+        if self.total_time >= 0:
+            self.total_time -= frame_time
 
-        if self.total_time < 0:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            target_pos = Vector2(mouse_x, mouse_y)  # Get the player position here
-            bullet_controller.add_bullet(self.bullet_type(position, (target_pos-position).normalize()))
+        if pygame.mouse.get_pressed()[0] == 1:
+            if self.total_time < 0:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                target_pos = Vector2(mouse_x, mouse_y)  # Get the player position here
+                bullet_controller.add_bullet(self.bullet_type(position, (target_pos-position).normalize()))
 
                 self.total_time += self.cooldown
